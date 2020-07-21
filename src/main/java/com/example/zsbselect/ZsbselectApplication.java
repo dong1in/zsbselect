b77c226346bf496044a75c17e5ccb142f3f1217e
dong1in <335188567@qq.com>
2020-07-21 09:51:10 +0800

    Initial commit

---
+++ src/main/java/com/example/zsbselect/ZsbselectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zsbselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsbselectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsbselectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zsbselect/ZsbselectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zsbselect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZsbselectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
